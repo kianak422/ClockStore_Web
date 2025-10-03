1deed7c72ce8793d6a87bf8390041a9772676261
kianak422 <huyway2005@gmail.com>
2025-10-03 10:36:39 +0000

    Mô tả thay đổi

--- ClockSln/ClockStore/Program.cs
+++ ClockSln/ClockStore/Program.cs
@@ -1,23 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using ClockStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(opts =>
+  opts.UseSqlServer(builder.Configuration["ConnectionStrings:ClockStoreConnection"]));
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
 app.UseStaticFiles();
+app.UseSession();
 
-app.UseRouting();
+app.MapControllerRoute("catpage", "{category}/Page{productPage:int}", new { Controller = "Home", action = "Index" });
+app.MapControllerRoute("page", "Page{productPage:int}", new { Controller = "Home", action = "Index", productPage = 1 });
+app.MapControllerRoute("category", "{category}", new { Controller = "Home", action = "Index", productPage = 1 });
+app.MapDefaultControllerRoute();
 
-app.UseAuthorization();
+app.MapRazorPages();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+SeedData.EnsurePopulated(app);
 
 app.Run();
