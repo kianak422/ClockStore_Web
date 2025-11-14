bf51f4de9621f9656c77923a0a35197f6a53ac46
kianak422 <huyway2005@gmail.com>
2025-11-14 17:20:24 +0700

    Delete ClockSln directory

--- ClockSln/ClockStore/Models/StoreDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-
-namespace ClockStore.Models {
-  public class StoreDbContext : DbContext {
-    public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
-    public ISet<Product> Products => Set<Product>();
-  }
-}
