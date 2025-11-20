5e74e051eca25c6a9a61317cfd5ffb8f90d5d37c
kianak422 <huyway2005@gmail.com>
2025-11-21 00:43:48 +0700

    Delete ClockSln directory

--- ClockSln/ClockStore/Models/ShoppingCartLine.cs
+++
@@ -1,9 +0,0 @@
-namespace ClockStore.Models
-{
-    public class ShoppingCartLine
-    {
-        public int CartLineID { get; set; }
-        public Clock? Clock { get; set; }
-        public int Quantity { get; set; }
-    }
-}
